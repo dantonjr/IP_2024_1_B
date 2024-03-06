5d3431bf59d43e2b497b69ccea80134c58ba5fd9
Danton Cavalcanti Franco Junior <falecom@dantonjr.com.br>
2024-03-06 18:36:01 -0300

    Postagem da Unidade 3

---
+++ Unidade1/src/OlaMundo.java
@@ -0,0 +1,10 @@
+public class OlaMundo {
+
+    public static void main (String [] args) {
+        System.out.println("Olá Mundo!!");
+        int x = 10;
+        int y = 40;
+        int soma = x + y;
+        System.out.println(soma);
+    }
+}
